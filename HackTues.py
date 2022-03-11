84b0a47c6925daff511b59a2f57647e11f75ec56
YavorPingvina <101359196+YavorPingvina@users.noreply.github.com>
2022-03-11 20:07:27 +0200

    Add files via upload

--- HackTues.py
+++ HackTues.py
@@ -1,40 +1,38 @@
 import pygame as pg
 import os
 import random
+import time
 
 pg.init()
 
-WIDTH, HEIGHT = 800, 500
-WIN = pg.display.set_mode((WIDTH, HEIGHT)) #create window
-pg.display.set_caption("SHTO NE TRUGWA") #set display caption
+class Planet:
+    def __init__(self, width, hight, gravity, air_pressure):
+        self.width = width
+        self.hight = hight
+        self.gravity = gravity
+        self.air_pressure = air_pressure
+gold, silver, oil, gold_needed, silver_needed, oil_needed, oxygen = 0, 0, 0, 0, 0, 0, 100
 
-def planet_creator():
-    width_planet = random.randint(400, 800)
-    height_planet = random.randint(250, 500)
-    PLANET = pg.image.load(os.path.join('images', 'planet.jpg'))
-    PLANET_RESIZE = pg.transform.scale(PLANET, (width_planet, height_planet))
-    WIN.blit(PLANET_RESIZE, (400, 250))
+width = random.randint()
+hight = random.randint()
+gravity = random.randint(1, 100)
+air_pressure = random.randint(1, 100)
 
-def main():
-
-    #closing the window
-    run = True 
-    while run:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                run = False
-    
-        #WIN.fill((255,255,255))
-        pg.display.update()
-        BACKGROUND = pg.image.load(os.path.join('images', 'space.jpg'))
-        BACKGROUND_RESIZE = pg.transform.scale(BACKGROUND, (800, 500))
-        WIN.blit(BACKGROUND_RESIZE, (0, 0))
-
-        #tuka shte se chekwa dali sa stignati neobhodimite resursi za da se otide na/naprawi nowa planeta 
-        planet_creator()
-        pg.display.update()
-        
-    pg.quit()
-
-
-main()
+if gold >= gold_needed and silver >= silver_needed and oil >= oil_needed:
+    planet = (width, hight, gravity, air_pressure)
+    if air_pressure > 80:
+        while oxygen > 0:
+            oxygen -= oxygen
+            time.sleep(1)
+    if air_pressure > 60:
+        while oxygen > 0:
+            oxygen -= oxygen
+            time.sleep(2)
+    if air_pressure > 40:
+        while oxygen > 0:
+            oxygen -= oxygen
+            time.sleep(3)
+    if air_pressure <= 40:
+        while oxygen > 0:
+            oxygen -= oxygen
+            time.sleep(4)
