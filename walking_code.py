793a03c2c4f43390dc9b1ec8d0ba4387723bd6bf
Vikcho TUES <vik.penev@gmail.com>
2022-03-11 10:48:17 +0200

    Made walking code to run more smoothly

--- walking_code.py
+++ walking_code.py
@@ -1,30 +1,49 @@
-import pygame 
-pygame.init()
+import pygame as pg, sys
 
-window = pygame.display.setImode((400, 400))
-pygame.display.set_caption("Movement Trail Base:")
+mainClock = pg.time.Clock()
 
-x = 40
-# y - 40
-width = 30
-height = 100
-vel = 5
+from pygame.locals import *
+pg.init()
 
-from pygame.locals import(
-    K_A,
-    K_D,
-    K_ESCAPE,
-    KEYDOWN,
-    QUIT,
-)
+screen = pg.display.set_mode((800, 600),0,32)
+background = pg.image.load("background1.png")
 
+pg.display.set_caption("BDSM")
+icon = pg.image.load("planet-earth.png")
+pg.display.set_icon(icon)
 
-walk  = True
+planetImg = pg.image.load("planet-earth (4).png")
+angle = 0
 
-while walk:
-    pygame.time.delay(100)
+running = True
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            walk = False
+while running:
+    pg.time.delay(100)
 
+    screen.fill((255, 255, 255))
+    screen.blit(background, (0, 0))
+
+    x = 370
+    y = 600
+    vel = 5
+
+    img_copy = pg.transform.rotate(planetImg, angle)
+    screen.blit(img_copy, (x - int(img_copy.get_width()/2),y - int(img_copy.get_height() / 2)))
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    keys = pg.key.get_pressed()
+
+    if event.type == pg.KEYDOWN:
+        if event.key == pg.K_LEFT:
+            angle -= vel
+        if event.key == pg.K_RIGHT:
+            angle += vel
+            img_copy = pg.transform.rotate(planetImg, angle)
+
+    pg.time.delay(100)
+    pg.display.update()
+
+pg.quit()
