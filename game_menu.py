f8a7637daf75e1976a50072d44658e0e8410257f
Vikcho TUES <vik.penev@gmail.com>
2022-03-11 15:08:03 +0200

    Changes to GAME MENU

--- game_menu.py
+++ game_menu.py
@@ -1,4 +1,5 @@
 import pygame as pg
+from menu import *
 
 
 class Game():
@@ -13,8 +14,13 @@ class Game():
         
         pg.display.set_caption("GAME MENU")
 
-        self.font_name = '8-BIT WONDER.TTF'
-        self.BLACK, self.WHITE = (0,0,0), (255, 255, 255)
+        self.font_name = 'Pixeboy.ttf'
+        self.BLACK, self.WHITE = (0, 0, 0), (255, 255, 255)
+ 
+        self.curr_menu = MainMenu
+        self.options = OptionsMenu(self)
+        self.credits = CreditsMenu(self)
+        self.curr_menu = self.main_menu
 
     def game_loop(self):
         while self.playing:
@@ -24,9 +30,9 @@ class Game():
                 self.playing = False
             
             self.display.fill(self.BLACK)
-            self.draw_text("NOT WORKING!!!", 20, self.DISPLAY_WIDTH/2, self.DISPLAY_HEIGHT/2)
-            self.window.blit(self.display, (0,0))
+            self.draw_text("GAME MENU", 50, self.DISPLAY_WIDTH/2, self.DISPLAY_HEIGHT/2)
             
+            self.window.blit(self.display, (0,0))
             pg.display.update()
             self.keys_reset()
 
@@ -34,6 +40,7 @@ class Game():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 self.running, self.playing = False, False
+                self.curr_menu.run_display = False
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_RETURN:
                     self.START_KEY = True
@@ -46,8 +53,9 @@ class Game():
     
     def keys_reset(self):
         self.START_KEY, self.BACK_KEY, self.K_DOWN, self.K_UP = False, False, False, False
+        
     def draw_text(self, text, size, x, y):
-        font = pg.font.Font(self.font_name,size)
+        font = pg.font.Font(self.font_name, size)
         text_surface = font.render(text, True, self.WHITE)
         text_rect = text_surface.get_rect()
         text_rect.center = (x,y)
---
+++ main.py
@@ -0,0 +1,7 @@
+from game_menu import Game
+
+g = Game()
+
+while g.running:
+    g.curr_menu.display_menu()
+    g.game_loop()
